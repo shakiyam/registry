df5b046fca600568df19c7008eac6e8b374c8180
Shinichi Akiyama <shinichi.akiyama@gmail.com>
2019-01-08 15:16:42 +0900

    Support Ubuntu

--- configure_client.sh
+++ configure_client.sh
@@ -12,7 +12,20 @@ fi
 [[ -n "${REGISTRY_OWNER:-}" ]] || { echo "REGISTRY_OWNER is not defined."; exit 1; }
 [[ -n "${REGISTRY_HOME:-}" ]] || { echo "REGISTRY_HOME is not defined."; exit 1; }
 
-sudo scp "${REGISTRY_OWNER}@${REGISTRY_HOST}:${REGISTRY_HOME}/certs/ca.crt" "/etc/pki/ca-trust/source/anchors/${REGISTRY_HOST}.crt"
-sudo update-ca-trust
+# shellcheck disable=SC1091
+os_id=$(. /etc/os-release; echo "$ID")
+
+case $os_id in
+  ol)
+    sudo scp "${REGISTRY_OWNER}@${REGISTRY_HOST}:${REGISTRY_HOME}/certs/ca.crt" "/etc/pki/ca-trust/source/anchors/${REGISTRY_HOST}.crt"
+    sudo update-ca-trust
+    ;;
+  ubuntu)
+    sudo scp "${REGISTRY_OWNER}@${REGISTRY_HOST}:${REGISTRY_HOME}/certs/ca.crt" "/usr/share/ca-certificates/${REGISTRY_HOST}.crt"
+    echo "${REGISTRY_HOST}.crt" | sudo tee -a /etc/ca-certificates.conf >/dev/null
+    sudo update-ca-certificates
+    ;;
+esac
+
 sudo systemctl restart docker
-docker login "${REGISTRY_HOST}" -p "${AUTH_PASSWORD}" -u "${AUTH_USERNAME}"
+echo "${AUTH_PASSWORD}" | docker login "${REGISTRY_HOST}" --password-stdin -u "${AUTH_USERNAME}"
